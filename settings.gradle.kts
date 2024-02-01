492ed8fc20a2e65b5ff0affc74fd3302eb72f429
kamilpierudzki <kamil.pierudzki@outlook.com>
2024-02-01 14:39:47 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "car-engine-check"
+include(":app")
+include(":obd-api")
+include(":legacy-app")
+include(":legacy-app-older")
